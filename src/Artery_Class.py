70305868e5570e641f69829abc738f0e44b99c3a
Chayut Teeraratkul <chte9199@colorado.edu>
2020-12-05 18:10:13 -0700

    Added many test cases

--- src/Artery_Class.py
+++ src/Artery_Class.py
@@ -23,40 +23,33 @@ class Artery:
         # -- Finite Element Space Data
         QE     = fe.FiniteElement("Lagrange", cell=self.mesh.ufl_cell(), degree=degQ)
         AE     = fe.FiniteElement("Lagrange", cell=self.mesh.ufl_cell(), degree=degA)
-        ME     = fe.MixedElement([QE,AE])
+        ME     = fe.MixedElement([AE,QE])
         self.W = fe.FunctionSpace(self.mesh,ME)
-        (self.w,self.q)            = fe.TestFunction(self.W)
-        (self.QTrial, self.ATrial) = fe.TrialFunction(self.W)
+        (self.vA    , self.vQ)     = fe.TestFunction(self.W)
+        (self.ATrial, self.QTrial) = fe.TrialFunction(self.W)
         # -- Coordinate values
         x = fe.interpolate(fe.Expression("x[0]", degree=1), self.W.sub(0).collapse())
         self.coords = self.gatherField(x)
-        print("rank",self.rank,"coord",self.coords)
+        # print("rank",self.rank,"coord",self.coords)
         # sys.exit()
         # -- Get beta and A
-        self.beta  = fe.interpolate(beta, self.W.sub(1).collapse())
-        self.A0    = fe.interpolate(A0, self.W.sub(1).collapse())
+        self.beta  = fe.interpolate(beta, self.W.sub(0).collapse())
+        self.A0    = fe.interpolate(A0, self.W.sub(0).collapse())
         self.dbeta = fe.grad( self.beta )[0]
         self.dA0   = fe.grad( self.A0   )[0]
         # -- Initialize solutions
-        self.currentSol  = fe.Function(self.W)
-        self.previousSol = fe.Function(self.W)
-        fe.assign(self.previousSol.sub(1),self.A0)
+        self.nextSol  = fe.Function(self.W)
+        self.currentSol = fe.Function(self.W)
+        fe.assign(self.currentSol.sub(0),self.A0)
         self.wf = self.getWeakform()
 
-        x2 = fe.interpolate(fe.Expression("x[0]*x[0]", degree=1), self.W.sub(0).collapse())
-        print(self.interpolate(x2,10))
-
-
-
-        # self.getBoundaryChar_Pos()
-        # self.getBoundaryChar_Neg()
 
     # -- Get boundary values
     def getBoundaryA(self):
-        (Q,A) = self.previousSol.split(deepcopy=True)
+        (A,Q) = self.currentSol.split(deepcopy=True)
         return self.getBoundaryValues(A) 
     def getBoundaryQ(self):
-        (Q,A) = self.previousSol.split(deepcopy=True)
+        (A,Q) = self.currentSol.split(deepcopy=True)
         return self.getBoundaryValues(Q) 
 
     def getBoundaryValues(self, field):
@@ -90,7 +83,6 @@ class Artery:
         if pt < self.coords[0] or pt > self.coords[-1]:
             raise Exception('Point out of bound.')
         field_gathered = self.gatherField(field) 
-        print("rank:",self.rank,"field:",field_gathered)
         for i in range(0,len(field_gathered)):
             if pt < self.coords[i]:
                 break
@@ -98,80 +90,135 @@ class Artery:
         f2 = field_gathered[i]
         x1 = self.coords[i-1]
         x2 = self.coords[i]
-        print(self.rank,"f",f1,f2)
-        print(self.rank,"x",x1,x2)
-        print(self.rank,"pt",pt)
-        print(self.rank,"i",i)
         a = (f2-f1)/(x2-x1)
         b = f1-a*x1
         return a*pt+b
 
 
 
-
-    # -- Get characteristics
-    def getBoundaryChar_Pos(self):
-        ''' Get the positive drisciminant of the characteristic solution '''
-        (Q,A) = self.previousSol.split(deepcopy=True)
-        alpha = Global.alpha
+    def getc1(self,beta,A0,A):
+        ''' Return c1 with values beta, A0, A '''
         rho = Global.rho
+        c1 = ( beta/(2*rho*A0) )**(0.5) * A**(0.25) 
+        return c1
+
+    def getAQFromChar(self, beta, A0, char_pos, char_neg):
+        rho = Global.rho 
+        A = (2*rho*A0/beta)**2 * ( (char_pos - char_neg)/8 )**4
+        Q = A/2*(char_pos - char_neg)
+        return (Q,A)
+
+    def getCharacteristic(self,beta,A0,Q,A):
+        ''' Compute characteristics at values beta, A0, Q, A postive and negative discriminants
+            return ( char+ , char- )'''
+        c1 = self.getc1(beta,A0,A)
+        char_pos = Q/A + 4*c1
+        char_neg = Q/A - 4*c1
+        return (char_pos, char_neg)
+
+    def getLambda(self,beta,A0,Q,A):
+        ''' Compute eigenvalues of H at values beta, A0, Q, A postive and negative discriminants
+            return ( lambda+, lambda- )'''
+        alpha = Global.alpha
+        c1 = self.getc1(beta,A0,A)
+        lambda_pos = alpha*Q/A + ( c1**2 + alpha*(alpha-1)*Q**2/A**2    )**(0.5)
+        lambda_neg = alpha*Q/A - ( c1**2 + alpha*(alpha-1)*Q**2/A**2    )**(0.5)
+        return (lambda_pos, lambda_neg)
+
+    def getBoundaryCharacteristic(self,LeftOrRight):
+        ''' Compute positive or negative discriminant boundary characteristics
+            return ( charLeft+  , charLeft-  ) if LeftOrRight=="left" 
+            return ( charRight+ , charRight- ) if LeftOrRight=="right"
+        '''
         (betaL , betaR) = self.getBoundaryValues(self.beta)
         (A0L   , A0R  ) = self.getBoundaryValues(self.A0)
         (QL    , QR   ) = self.getBoundaryQ()
         (AL    , AR   ) = self.getBoundaryA()
-        charL_Pos = alpha*QL/AL + 4*AL**(1/4)*(betaL/(2*rho*A0L))**(1/2)
-        charR_Pos = alpha*QR/AR + 4*AR**(1/4)*(betaR/(2*rho*A0R))**(1/2)
-        return [charL_Pos, charR_Pos]
+        if   LeftOrRight.lower() == "left":
+            (char_pos, char_neg) = self.getCharacteristic(betaL, A0L, QL, AL)
+        elif LeftOrRight.lower() == "right":
+            (char_pos, char_neg) = self.getCharacteristic(betaR, A0R, QR, AR)
+        else:
+            raise ValueError("LeftOrRight must be either \"left\" or \"right\"")
+        return (char_pos, char_neg)
 
-    def getBoundaryChar_Neg(self):
-        ''' Get the positive drisciminant of the characteristic solution '''
-        (Q,A) = self.previousSol.split(deepcopy=True)
-        alpha = Global.alpha
-        rho = Global.rho
+    def getBoundaryLambda(self,LeftOrRight):
+        ''' Compute Left or Right boundary characteristics
+            return (lambdaLeft+ , lambdaLeft- ) if LeftOrRight=="left" 
+            return (lambdaRight+, lambdaRight-) if LeftOrRight=="right"
+        '''
         (betaL , betaR) = self.getBoundaryValues(self.beta)
         (A0L   , A0R  ) = self.getBoundaryValues(self.A0)
         (QL    , QR   ) = self.getBoundaryQ()
         (AL    , AR   ) = self.getBoundaryA()
-        charL_Neg = alpha*QL/AL - 4*AL**(1/4)*(betaL/(2*rho*A0L))**(1/2)
-        charR_Neg = alpha*QR/AR - 4*AR**(1/4)*(betaR/(2*rho*A0R))**(1/2)
-        return [charL_Neg, charR_Neg]
+        if   LeftOrRight.lower() == "left":
+            (lambda_pos, lambda_neg) = self.getLambda(betaL, A0L, QL, AL)
+        elif LeftOrRight.lower() == "right":
+            (lambda_pos, lambda_neg) = self.getLambda(betaR, A0R, QR, AR)
+        else:
+            raise ValueError("LeftOrRight must be either \"left\" or \"right\"")
+        return (lambda_pos, lambda_neg)
+
+
+    def getCharacteristicAtPt(self, pt):
+        ''' Return characteristics postive and negative at a designated point '''
+        (ACurrent,QCurrent) = self.currentSol.split(deepcopy=True)
+        beta = self.interpolate( self.beta    , pt )
+        A0   = self.interpolate( self.A0      , pt )
+        A    = self.interpolate( ACurrent     , pt )
+        Q    = self.interpolate( QCurrent     , pt )
+        (char_pos, char_neg) = self.getCharacteristic(beta, A0, Q, A)
+        return (char_pos, char_neg)
+
+    def getLambdaAtPt(self, pt):
+        ''' Return eigenvalues of H positive and negative at a designated point '''
+        (ACurrent,QCurrent) = self.currentSol.split(deepcopy=True)
+        beta = self.interpolate( self.beta    , pt )
+        A0   = self.interpolate( self.A0      , pt )
+        A    = self.interpolate( ACurrent     , pt )
+        Q    = self.interpolate( QCurrent     , pt )
+        (lambda_pos, lambda_neg) = self.getLambdaAtPt(beta, A0, Q, A)
+        return (lambda_pos, lambda_neg)
 
 
     def getWeakform(self):
         # -- Weakform data 
-        w = self.w
-        q = self.q
-        dw = fe.grad(w)[0]
-        dq = fe.grad(q)[0]
+        vA = self.vA
+        vQ = self.vQ
+        dvA = fe.grad(vA)[0]
+        dvQ = fe.grad(vQ)[0]
         dt = Global.dt
-        (Q0,A0) = fe.split(self.previousSol)
-        (Qn,An) = fe.split(self.currentSol)
+        (A,Q) = fe.split(self.currentSol)
         # -- Define weakform term
-        F = self.getF(Q0,A0)
-        B = self.getB(Q0,A0)
-        H = self.getH(Q0,A0)
-        FLW = self.getFLW(Q0,A0)
-        BLW = self.getBLW(Q0,A0)
-        BU = self.getBU(Q0,A0)
+        F   = self.getF   (Q,A)
+        B   = self.getB   (Q,A)
+        H   = self.getH   (Q,A)
+        FLW = self.getFLW (Q,A)
+        BLW = self.getBLW (Q,A)
+        BU  = self.getBU  (Q,A)
         dFdz = [fe.grad(F[0])[0], fe.grad(F[1])[0]]
         BU_dFdz = self.matMult(BU,dFdz)
         H_dFdz = self.matMult(H,dFdz)
         # -- Trial term
-        wf  = -(self.QTrial*w + self.ATrial*q)
-        # -- Q term
-        wf += Q0*w
-        wf += dt*FLW[0]*dw
-        wf += -dt**2/2 * BU_dFdz[0]*w
-        wf += -dt**2/2 * H_dFdz[0]*dw
-        wf += dt*BLW[0]*w
+        wf  = -(self.QTrial*vQ + self.ATrial*vA)
         # -- A term
-        wf += A0*q
-        wf += dt*FLW[1]*dq
-        wf += -dt**2/2 * BU_dFdz[1]*q
-        wf += -dt**2/2 * H_dFdz[1]*dq
-        wf += dt*BLW[1]*q
-        wf *= fe.dx
-        self.wf = wf
+        wf += A*vA
+        wf += Q*vQ
+
+        wf +=  dt*FLW[0]*dvA
+        wf +=  dt*FLW[1]*dvQ
+
+        wf += -dt**2/2 * BU_dFdz[0]*vA
+        wf += -dt**2/2 * BU_dFdz[1]*vQ
+
+        wf += -dt**2/2 * H_dFdz[0]*dvA
+        wf += -dt**2/2 * H_dFdz[1]*dvQ 
+
+        wf +=  dt * BLW[0]*vA 
+        wf +=  dt * BLW[1]*vQ
+
+        wf *=  fe.dx
+        return wf
 
 
     def getF(self,Q,A):
@@ -189,7 +236,7 @@ class Artery:
         beta = self.beta
         dbeta = self.dbeta
         dA0 = self.dA0
-        B = [0,KR*Q/A + A/(A0*rho)*(2/3*A**(1/2)-A0**(1/2))*dbeta - beta/rho*A/A0**2*(2/3*A**(1/2)-1/2*A0**(1/2))*dA0]
+        B = [fe.Constant(0),KR*Q/A + A/(A0*rho)*(2/3*A**(1/2)-A0**(1/2))*dbeta - beta/rho*A/A0**2*(2/3*A**(1/2)-1/2*A0**(1/2))*dA0]
         return B
 
     def getH(self,Q,A):
@@ -197,7 +244,7 @@ class Artery:
         rho = Global.rho
         beta = self.beta
         A0 = self.A0
-        H = [ [0,1] , [-alpha*Q**2/A**2 + beta/(2*rho*A0)*A**(1/2), 2*alpha*Q/A]  ]
+        H = [ [fe.Constant(0), fe.Constant(1)] , [-alpha*Q**2/A**2 + beta/(2*rho*A0)*A**(1/2), 2*alpha*Q/A]  ]
         return H
 
     def getBU(self,Q,A):
@@ -209,9 +256,9 @@ class Artery:
         dA0 = self.dA0
         BU1  = -KR*Q/A**2 
         BU1 += (beta*dA0 - 2*A0*dbeta)/(2*rho*A0**(3/2)) 
-        BU1 += (A**(1/2)/rho*A0**2) * (-beta*dA0 + A0*dbeta)
+        BU1 += (  A**(1/2)/(rho*A0**2) * ( -beta*dA0 + A0*dbeta   )   )
         BU2 = KR/A
-        BU = [ [0,0] , [BU1, BU2] ]
+        BU = [ [fe.Constant(0), fe.Constant(0)] , [BU1, BU2] ]
         return BU
 
     def getFLW(self, Q, A):
@@ -243,10 +290,7 @@ class Artery:
         return [ A[0][0]*x[0] + A[0][1]*x[1] , A[1][0]*x[0] + A[1][1]*x[1] ] 
 
     def updateSol(self):
-        self.previousSol.assign(self.currentSol)
-
-
-    
+        self.currentSol.assign(self.nextSol)
 
     def plotMesh(self):
         fe.plot(self.mesh)
--- src/Global.py
+++ src/Global.py
@@ -11,12 +11,14 @@ KR = 8*nu*pi
 # -- Time parameters
 T = 2*0.165
 nt = 1e3
-t = np.linspace(0,T/2+(0.25-0.165),int(nt))
-dt = t[1]-t[0]
+time = np.linspace(0,T/2+(0.25-0.165),int(nt))
+# dt = 0.001
+dt = time[1]-time[0]
+# print(dt)
 
-def P2A(beta, A0, A):
+def A2P(beta, A0, A):
     P = beta/A0 * (A**(1/2) - A0**(1/2) ) 
     return P
-def A2P(beta, A0, P):
+def P2A(beta, A0, P):
     A = ( A0/beta*P + A0**(1/2) )**2
     return A
