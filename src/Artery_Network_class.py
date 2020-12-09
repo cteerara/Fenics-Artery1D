Fixed bug in K computation in main

src/Artery_Network_class.py
@@ -35,20 +35,45 @@ class Artery_Network:
             degQ = self.paramDict["degQ"][i]
             Arty = Artery(L,ne,r0,Q0,E,h0,theta,dt,degA=degA,degQ=degQ)
             self.Arteries.append(Arty)
+            
 
-        self.getNRSystem([1,2,3],[4,5,6])
 
     
-    def getNRSystem(self,A,Q):
+    def getNRSystem(self,A,Q,gamma,sigma):
+        ''' A and Q are vectors containing boundary values of A and Q to be solved in the compatibility condition  '''
+        print("Avec",A)
+        print("Qvec",Q)
+        print("GammeVec",gamma)
+        print("SigmaVec",sigma)
+        print()
+
         n = len(A)-1
-        Ai = A[1:] ; Qi = Q[1:] # There are n members in these arrays
         K = np.zeros( (2*n+2,2*n+2) )
         R = np.zeros( 2*n+2 )
         Ap = A[0] ; Qp = Q[0]
+        gammap = gamma[0]  ; sigmap = sigma[0]
         K[0,0] = 1
         K[0,1:n+1] = -1
-        K[1:n+1,0] = Qp/Ap**2
+        # K[1:n+1,0] = Qp/Ap**2
         K[n+1,0] = alpha/Ap
+        print(alpha/Ap)
+        # K[1:n+1,n+1] = -Qp**2/Ap**3 + gammap/(2*np.sqrt(Ap)) 
+        # K[n+1,n+1] = -alpha*Qp/Ap**2 + sigmap/(2*Ap**(3/4))
+        # Ki11 = []
+        # Ki12 = []
+        # Ki21 = []
+        # Ki22 = []
+        # for i in range(1,n+1):
+        #     Ki11.append( -Q[i]/A[i]**2  )
+        #     Ki12.append( Q[i]**2/A[i]**3 - gamma[i]/(2*np.sqrt(A[i])) )
+        #     Ki21.append( alpha/A[i] )
+        #     Ki22.append( -alpha*Q[i]/A[i]**2 - sigma[i]/(4*A[i]**(3./4)) )
+        # K[1:n+1,1:n+1] = Ki11
+        # K[1:n+1,n+2:2*n+2] = Ki12
+        # K[n+2:2*n+2,1:n+1] = Ki21
+        # K[n+2:2*n+2,1:n+1] = Ki22
+        return K
+
 
 
 
@@ -116,6 +141,19 @@ class Artery_Network:
 
 
 inputFile = 'testInput.in'
+inputFile = 'TwoSection.in'
 A = Artery_Network(inputFile,1,1)
+(A_p, Q_p) = A.Arteries[0].getBoundaryAQ("right")
+(A_1, Q_1) = A.Arteries[1].getBoundaryAQ("left") 
+(lam1_p, lam2_p) = A.Arteries[0].getEigenvalues(A_p,Q_p)
+(lam1_1, lam2_1) = A.Arteries[1].getEigenvalues(A_1,Q_1)
+gamma_p = A.Arteries[0].beta / A.Arteries[0].A0
+gamma_1 = A.Arteries[1].beta / A.Arteries[1].A0
+sigma_p = 4 * np.sqrt(A.Arteries[0].beta/(2*rho*A.Arteries[0].A0))
+sigma_1 = 4 * np.sqrt(A.Arteries[1].beta/(2*rho*A.Arteries[1].A0))
+
+
+K = A.getNRSystem([A_p,A_1], [Q_p,Q_1], [gamma_p,gamma_1], [sigma_p,sigma_1])
+print(K)
 # print(A.numVessels, A.connectivity, A.vesselIDs)
 

src/main.py
@@ -37,7 +37,7 @@ degA = 1
 degQ = 1
 
 Artyp = Artery(L, ne, r0, Q0,   E, h0, theta, dt, degA=degA,degQ=degQ)
-Arty1 = Artery(L, ne, r0, Q0, 1*E, h0, theta, dt, degA=degA,degQ=degQ)
+Arty1 = Artery(L, ne, r0, Q0, 1.5*E, h0, theta, dt, degA=degA,degQ=degQ)
 
 # -- Constants
 beta_p = Artyp.beta
@@ -48,6 +48,7 @@ gamma_p= beta_p/A0_p
 gamma_1= beta_1/A0_1
 sigma_p= 4*np.sqrt(beta_p/(2*rho*A0_p))
 sigma_1= 4*np.sqrt(beta_1/(2*rho*A0_1))
+
 # -- Stiffness matrix
 K = np.zeros((4,4))
 R = np.zeros(4)
@@ -80,7 +81,7 @@ for t in time:
         K[1,1]=-Q_1/A_1**2
         K[1,2]=-Q_p**2/A_p**3+gamma_p/(2*np.sqrt(A_p))
         K[1,3]=Q_1**2/A_1**2-gamma_1/(2*np.sqrt(A_1))
-        K[2,0]=alpha*A_p
+        K[2,0]=alpha/A_p
         K[2,2]=-alpha*Q_p/A_p**2+sigma_p/(4*A_p**(3/4))
         K[3,1]=alpha/A_1
         K[3,3]=-alpha*Q_1/A_1**2-sigma_1/(4*A_1**(3/4))
@@ -88,6 +89,8 @@ for t in time:
         R[1] = ptp - pt1 
         R[2] = alpha*Q_p/A_p + sigma_p*A_p**(1/4) - W1_p
         R[3] = alpha*Q_1/A_1 - sigma_1*A_1**(1/4) - W2_1
+        # print(K)
+        # sys.exit('')
         print("Time step: %d. NR iteration: %d. Residue = %f" % (tid, NR_it, np.linalg.norm(R)))
         if np.linalg.norm(R) < tol:
             break